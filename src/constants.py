90f077ae555a49e2d2312b94d8650629dd30e7e3
Форов Александр <5h9t5@list.ru>
2024-06-30 19:33:56 +0300

    add main logic

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+MAIN_DOC_URL_PEP = 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
