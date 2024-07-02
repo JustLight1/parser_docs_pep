7061833560bf994890eede4e21e988f63fc54f74
Форов Александр <5h9t5@list.ru>
2024-07-02 18:50:17 +0300

    add const

--- src/constants.py
+++ src/constants.py
@@ -7,4 +7,17 @@ BASE_DIR = Path(__file__).parent
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
 DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+FIRST_TD = 0
 SECOND_TD = 1
+STATUS = 1
+
+EXPECTED_STATUS = {
+    'A': ['Active', 'Accepted'],
+    'D': ['Deferred'],
+    'F': ['Final'],
+    'P': ['Provisional'],
+    'R': ['Rejected'],
+    'S': ['Superseded'],
+    'W': ['Withdrawn'],
+    '': ['Draft', 'Active'],
+}
